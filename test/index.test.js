03262743ed66a9db1b4850cc11cae9883c29fcd4
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-02-02 09:02:41 -0500

    test: :white_check_mark: Adds tests for deepAssign package
    initial tests just to handle simple and complex depth, need to add coverage for other types like arrays

---
+++ test/index.test.js
@@ -0,0 +1,76 @@
+import test from 'ava';
+
+import { deepAssign } from '../src/deepAssign.js';
+
+test('deepAssign: Simple depth', t => {
+    const deepLevel1 = {
+        a: 1,
+        b: {
+            c: 2
+        }
+    }
+
+    const assignLevel1 = {
+        a: 1,
+        b: {
+            d: 3
+        }
+    };
+
+    const expectLevel1 = {
+        a: 1,
+        b: {
+            c: 2,
+            d: 3
+        }
+    };
+
+    const result = deepAssign(deepLevel1, assignLevel1);
+    
+    t.deepEqual(result, expectLevel1, 'Is this meant to only show up when there is an error?');
+});
+
+test('deepAssign: Complex depth', t => {
+    const original = {
+        a: 1,
+        b: {
+            c: 2,
+            d: {}
+        }
+    }
+
+    const assign = {
+        a: 1,
+        b: {
+            e: {
+                f: 3,
+                g: {
+                    h: [],
+                    i: '',
+                }
+            }
+        }
+    };
+
+    const expect = {
+        a: 1,
+        b: {
+            c: 2,
+            d: {},
+            e: {
+                f: 3,
+                g: {
+                    h: [],
+                    i: ''
+                }
+            }
+        }
+    };
+
+    const result = deepAssign(original, assign);
+
+    t.deepEqual(result, expect);
+})
+
+test.todo('deepAssign: Multiple arguments');
+test.todo('BabyStore: Test in browser');
