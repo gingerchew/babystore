4b1a4abee97cb0837d20943782887210e94f94e9
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-02-02 09:01:56 -0500

    fix: :bug: Fixes issue with objects not being deeply assigned
    was not properly looking for Object type

--- src/deepAssign.js
+++ src/deepAssign.js
@@ -1,4 +1,4 @@
-export const deepAssign = (orig,...args) => {
+const deepAssign = (orig,...args) => {
 
 	// Make sure there are objects to merge
 	let i = 0, l = args.length, next;
@@ -8,7 +8,14 @@ export const deepAssign = (orig,...args) => {
         for(;i<l;i++) 
             // If it's an object, recursively merge
             // Otherwise, push to key
-            for (let key in next=args[i]) orig[key] = toString.call(next[key])=='O' ? deepAssign(orig[key] || {}, next[key]) : next[key];
+            for (let key in next = args[i]) 
+                orig[key] = toString.call(next[key])[8]=='O' 
+                    ? deepAssign(orig[key] || {}, next[key]) 
+                    : next[key];
 
 	return orig;
+}
+
+export {
+    deepAssign
 }
