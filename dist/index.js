56b20c6dfe75149edb7ff4d4bbebd61e441bc96a
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-11-07 12:46:13 -0500

    Latest Build

--- dist/index.js
+++ dist/index.js
@@ -14,24 +14,29 @@ var J = JSON;
 var p = (v) => {
   try {
     v = J.parse(v);
-  } catch (e) {
-    v = v;
+  } catch {
   } finally {
     return v;
   }
 };
 var qd = (result, key) => result == null ? result : toString.call(result)[8] == "O" && key in result ? result[key] : result;
 var $$ = {
-  find: (key, ...keys) => key in lS && keys.reduce(qd, J.parse(lS[key])),
+  find: (key, ...keys) => key in lS && keys.length ? keys.reduce(qd, J.parse(lS[key])) : lS[key],
   add: (key, obj) => lS[key] = J.stringify(key in lS ? deepAssign(p(lS[key]), obj) : obj),
   delete: (key) => lS.removeItem(key),
   clear: () => lS.clear(),
   has: (key) => key in lS,
-  all: () => Array.from(lS, (_n, i) => p(lS.getItem(lS.key(i))))
+  all: () => Array.from(lS, (_n, i) => p(lS.getItem(lS.key(i) || "")))
 };
 var s = ($) => new Proxy($$, {
   apply: (_target, fn, [key, obj = {}]) => (key = $ == undef || key == undef ? key : $ + key, $$[fn](key, obj))
 });
+var a = ($) => new Proxy($$, {
+  apply: (_target, fn, [key, obj = {}]) => (key = $ == undef || key == undef ? key : $ + key, () => Promise.resolve($$[fn](key, obj)))
+});
+var store = s;
+var storeAsync = a;
 export {
-  s as default
+  store,
+  storeAsync
 };
