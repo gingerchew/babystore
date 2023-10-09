2d0fc5f8b74169a0c4a81f71956606be50f115e8
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-09 10:04:40 -0400

    Changes how loops are done, adds better types
    swaps a tradition `for(let i=0...)` loop for a for of loop.
    
    ```js
    for (let next of args) { ... }
    ```
    saves 12 bytes

--- src/deepAssign.ts
+++ src/deepAssign.ts
@@ -1,16 +1,17 @@
-import { DeepAssign } from '../types';
+import { DeepAssign, PotentialObject } from '../types';
+
 
 /** @type {import("../types").DeepAssign} */
-export const deepAssign:DeepAssign = (orig,...args) => {
+export const deepAssign:DeepAssign = (orig,...args:PotentialObject[]) => {
 
 	// Make sure there are objects to merge
 	if (args.length)
         // @TODO: Could this be made smaller with a for of loop
         // Merge all objects into first
-        for(let i=0,next,key;i<args.length;i++) 
+        for(let next of args) 
             // If it's an object, recursively merge
             // Otherwise, push to key
-            for (key in next = args[i]) 
+            for (let key in next) 
                 orig[key] = toString.call(next[key])[8]=='O'
                     ? deepAssign(orig[key] || {}, next[key]) 
                     : next[key];
