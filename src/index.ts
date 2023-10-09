ce57989034c835f28a419d06b5b993fb06d76ff4
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-09 00:53:47 -0400

    Changes to the way store is handled
    removed apply handler for a get handler that returns
    a function
    
    async is no longer an async function but a "nextTick" solution
    
    ```js
    const q = Promise.resolve();
    
    /**
     * this can be await'ed
     * and is async in the event loop
     */
    q.then(thing)
    ```
    
    also updates to a catchless try,finally

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,17 @@
 import { deepAssign } from './deepAssign.js';
 import { _UnknownObject, babystore, ReduceableObject } from '../types';
 
-let undef:undefined,
-    lS=localStorage,
+let lS=globalThis.localStorage,
     J=JSON,
     p=(v:string|unknown):unknown=>{
         try {
             // @ts-ignore
             v=J.parse(v);
-        }catch{}finally{
+        }finally{
             return v;
         }
     },
-
+    q = Promise.resolve(),
     qd = (result:ReduceableObject, key:string) => result == null
         ? result
         : (
@@ -25,32 +24,28 @@ let undef:undefined,
             : result,
 
     $$ = {
-        find: (key:string, ...keys:string[]) => key in lS && keys.length ? keys.reduce(qd, J.parse(lS[key])) : lS[key],
-        add: (key:string, obj:_UnknownObject) => lS[key] = J.stringify(
-            key in lS ? 
-            deepAssign(
-                // @ts-ignore
-                p(lS[key]),
-                obj
-            ) : obj
-        ),
+        find: (key:string, ...keys:string[]) => (key in lS && keys.length) ? keys.reduce(qd, p(lS[key])) : p(lS[key]),
+        add: (key:string, obj:_UnknownObject) => {
+            lS[key] = J.stringify(
+                key in lS ? 
+                deepAssign(
+                    // @ts-ignore
+                    p(lS[key]),
+                    obj
+                ) : obj
+            )
+        },
         delete: (key:string) => lS.removeItem(key),
         clear: () => lS.clear(),
         has: (key:string) => key in lS,
         all: () => Array.from(lS, (_n, i) => p(lS.getItem(lS.key(i) || ''))),
     },
-    s = ($:string) => new Proxy<babystore>($$, {
-            apply: (_target, fn, [key, obj={}]) => (key = $==undef||key==undef?key:$+key, $$[fn](
-                key,
-                obj
-            ))
-        }), 
-    a = ($:string) => new Proxy<babystore>($$, {
-            apply: (_target, fn, [key, obj={}]) => (key = $==undef||key==undef?key:$+key, () => Promise.resolve($$[fn](
-                key,
-                obj
-            )))
-        });
+    s = ($='') => new Proxy<babystore>($$, {
+        get: (_, fn:string) => (key?:string, obj?: object) => _[fn]($+key, obj)
+    }), 
+    a = ($='') => new Proxy<babystore>($$, {
+        get: (_, fn:string) => (key:string, obj = {}) => q.then(() => _[fn]($+key, obj)),
+    });
 
 // doing it this way brings down the esbuild package size
 export const store = s;
